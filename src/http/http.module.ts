8cb6abb0b41b7e1a6b2d2598cf62c161edd309f9
shardAstronaut <127413534+shardAstronaut@users.noreply.github.com>
2023-03-16 15:58:44 +0100

    feat: add http module

---
+++ src/http/http.module.ts
@@ -0,0 +1,22 @@
+import { APP_GUARD } from '@nestjs/core';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+
+import { HEALTH_URL } from 'common/health';
+import { METRICS_URL } from 'common/prometheus';
+
+import { SWAGGER_URL } from './common/swagger';
+import { ThrottlerModule, ThrottlerBehindProxyGuard } from './common/throttler';
+import { MetricsMiddleware } from './common/middleware';
+
+@Module({
+  imports: [ThrottlerModule],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerBehindProxyGuard }],
+})
+export class HTTPModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(MetricsMiddleware)
+      .exclude(`${SWAGGER_URL}/(.*)`, SWAGGER_URL, METRICS_URL, HEALTH_URL)
+      .forRoutes('*');
+  }
+}
