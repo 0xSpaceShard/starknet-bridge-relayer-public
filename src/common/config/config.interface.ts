578f58ea39eef9781b193052a91d748bdd6bbb47
shardAstronaut <127413534+shardAstronaut@users.noreply.github.com>
2023-03-16 15:47:23 +0100

    feat: add config module

---
+++ src/common/config/config.interface.ts
@@ -0,0 +1,17 @@
+export enum Environment {
+  development = 'development',
+  production = 'production',
+}
+
+export enum LogLevel {
+  error = 'error',
+  warning = 'warning',
+  notice = 'notice',
+  info = 'info',
+  debug = 'debug',
+}
+
+export enum LogFormat {
+  json = 'json',
+  simple = 'simple',
+}
