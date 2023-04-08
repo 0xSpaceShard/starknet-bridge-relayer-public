7be3c4e6917a04391b79d10765dd414ccf983720
shardAstronaut <127413534+shardAstronaut@users.noreply.github.com>
2023-04-08 12:07:56 +0100

    fix(e2e): add new dump data, improved the e2e tests and clean e2e folder

--- e2e/starknet-core/scripts/start.sh
+++ e2e/starknet-core/scripts/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-yarn e2e:node --fork ${RPC_URL} & sleep 5 && yarn e2e:setup && tail -f /dev/null
+source ../../e2e/e2e.env
+yarn && yarn typechain && yarn e2e:node & sleep 15 && yarn e2e:setup && tail -f /dev/null
