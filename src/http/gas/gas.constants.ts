735b312e67f5196a9bb49ddfe13b9d1e3b37f737
shardAstronaut <127413534+shardAstronaut@users.noreply.github.com>
2023-05-01 15:46:30 +0100

    feat(gas): add gas module constants

---
+++ src/http/gas/gas.constants.ts
@@ -0,0 +1,16 @@
+export const EtherscanApiUrl = (network: string): string=>  {
+    switch (network) {
+        case 'mainnet':
+            return 'https://api.etherscan.io/api'
+        default:
+            return 'https://api-goerli.etherscan.io/api'
+    }
+};
+export const OneGwei = 1000000000;
+export const FiveGwei = 5000000000;
+export const GasCostPerWithdrawal = 65000;
+export const BlockNumber24H = 5760; // 84600 / 14
+
+export const CacheFeeHistoryKey = 'feeHistory'
+export const CacheDuration24hInMs = 86400000 // 1000 * 60 * 60 * 24
+export const CheckPointSizeMs = 900
