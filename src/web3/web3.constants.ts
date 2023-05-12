58cf8ea9afb6f8ed4b6598dfddbe22de4f54745c
shardAstronaut <127413534+shardAstronaut@users.noreply.github.com>
2023-05-12 13:59:33 +0100

    feat(web3): implement multicall withh gas limit logic

--- src/web3/web3.constants.ts
+++ src/web3/web3.constants.ts
@@ -5,10 +5,12 @@ import { Provider } from './web3.interface';
 export const ADDRESSES: NetworkContractAddress = {
   mainnet: {
     multicall: '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696',
+    multicallGasLimit: '',
     starknetCore: '0xc662c410C0ECf747543f5bA90660f6ABeBD9C8c4',
   },
   goerli: {
     multicall: '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696',
+    multicallGasLimit: '0x07A9972b776Baf24DC9b2E3f7C55Da53e4F653CF',
     starknetCore: '0xde29d060D45901Fb19ED6C6e959EB22d8626708e',
   },
 };
@@ -20,5 +22,4 @@ export const getProviderURLs = (configs: ConfigService): Array<Provider> => {
   ];
 };
 
-export const GAS_LIMIT_PER_WITHDRAWAL = 100000
-export const GAS_LIMIT_MULTIPLE_WITHDRAWAL = 50000
+export const GAS_BUFFER_PER_WITHDRAWAL = 10000
