b5857685f07d34c33bf84b74990cbeff9dde1237
shardAstronaut <127413534+shardAstronaut@users.noreply.github.com>
2023-05-13 16:17:26 +0100

    chore: set mainnet multicall address

--- src/web3/web3.constants.ts
+++ src/web3/web3.constants.ts
@@ -5,7 +5,7 @@ import { Provider } from './web3.interface';
 export const ADDRESSES: NetworkContractAddress = {
   mainnet: {
     multicall: '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696',
-    multicallGasLimit: '',
+    multicallGasLimit: '0x3BEc1Fc3556cbdfd9Ebf9Ff398EF0434f1D7aD03',
     starknetCore: '0xc662c410C0ECf747543f5bA90660f6ABeBD9C8c4',
   },
   goerli: {
