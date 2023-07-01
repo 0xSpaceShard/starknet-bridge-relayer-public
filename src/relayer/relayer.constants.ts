6d5ea7c2b7a5feb132fe58277abdd63fbe3b36cf
shardAstronaut <127413534+shardAstronaut@users.noreply.github.com>
2023-07-01 11:45:34 +0100

    fix: bugs - increase the gas attached when call withdraw - fix the condition to check network fee notification

--- src/relayer/relayer.constants.ts
+++ src/relayer/relayer.constants.ts
@@ -1,7 +1,7 @@
-import { BigNumber } from "ethers";
+import { ethers } from "ethers";
 
 export const ZeroBytes = '0x0000000000000000000000000000000000000000000000000000000000000000';
 export const TRANSFER_FROM_STARKNET = 0;
 export const NumberOfWithdrawalsToProcessPerTransaction = 50
 export const NumberOfMessageToProcessPerTransaction = 50
-export const MinimumEthBalance = BigNumber.from(5e17.toString()) // 0.5 ETH
+export const MinimumEthBalance = ethers.utils.parseEther("1") // 1 ETH
